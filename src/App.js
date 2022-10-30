c97b9815df7be4009c74d5c1b077efdc747ead02
Julio Facal <starlyn.dev@gmail.com>
2022-10-30 10:07:58 -0300

    importando React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ListaCursos from './components/listaCursos';
 import Footer from './components/footer';
+import React from 'react'
 function App() {
   return (
     <div className="App">
